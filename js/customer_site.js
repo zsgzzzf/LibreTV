f234b5ed73b047e7ebfee61a94f75d141cd64771
zsgzzzf <52619792+zsgzzzf@users.noreply.github.com>
2026-03-07 17:07:21 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -7,9 +7,53 @@ const CUSTOMER_SITES = {
         api: 'https://moduzy.com/api.php/provide/vod',
         name: '魔都资源',
     },
-    mozhua: {
+    xiaomianyang: {
+        api: 'https://vs.okcdn100.top/api.php/provide/vod',
+        name: '小绵羊资源',
+    },
+    moDou: {
+        api: 'https://moduzy.com/api.php/provide/vod',
+        name: '魔都资源',
+    },
+    mozhuazy: {
         api: 'https://mozhuazy.com/api.php/provide/vod',
         name: '魔爪资源',
+    },
+    maotai: {
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        name: '茅台',
+    },
+    yutu: {
+        api: 'https://apiyutu.com/api.php/provide/vod',
+        name: '玉兔',
+    },
+    xiaojizy: {
+        api: 'https://api.xiaojizy.live/provide/vod?ac=list',
+        name: '小鸡资源',
+    },
+    rycj: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+    },
+    fqzy: {
+        api: 'https://fqzy.me/api.php/provide/vod',
+        name: '番茄资源',
+    },
+    doubanzy: {
+        api: 'https://doubanzy.cc/api.php/provide/vod',
+        name: '豆瓣资源',
+    },
+    iqiyizy: {
+        api: 'https://iqiyizyapi.com/api.php/provide/vod',
+        name: '爱奇艺资源',
+    },
+    dyttzy: {
+        api: 'http://dyttzy5.tv/api.php/provide/vod',
+        name: '电影天堂',
+    },
+    wolongzy: {
+        api: 'http://wolongzy.cc/api.php/provide/vod',
+        name: '卧龙资源',
     },
 };
 
